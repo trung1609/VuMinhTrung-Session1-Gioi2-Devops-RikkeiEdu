9dcdc915c20e14ab48825a6baef0a4e4fc3100f1
trung1609 <vmt162005@gmail.com>
2026-06-24 09:04:16 +0700

    update

--- homework/session_01/exercise_04/setup-dirs.sh
+++ homework/session_01/exercise_04/setup-dirs.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 sudo mkdir -p /opt/quickbite/user-service
-sudo groupadd quickbite
-sudo useradd -r -g quickbite -s /bin/false quickbite
 sudo chown -R quickbite:quickbite /opt/quickbite
-sudo chmod -R 750 /opt/quickbite
+sudo chmod 750 /opt/quickbite
+sudo chmod -R 750 /opt/quickbite/user-service
